082784db2c134b339a4eb40f0cb54ff73b8d60a4
mkuehl <maxmimilianlkuehl@web.de>
2015-10-28 18:13:35 +0100

    Variant06

--- Notepad.java
+++ Notepad.java
@@ -293,6 +293,55 @@ public class  Notepad  extends JFrame {
         center.nCenter();
         show();
     
+		ediT.add(cuT  = new JMenuItem("Cut",  new ImageIcon(this.getClass().getResource("images/cut.gif"))));
+		ediT.add(copY = new JMenuItem("Copy", new ImageIcon(this.getClass().getResource("images/copy.gif"))));
+		ediT.add(pastE= new JMenuItem("Paste",new ImageIcon(this.getClass().getResource("images/paste.gif"))));
+
+		cuT.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+		copY.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+		pastE.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+
+		toolBar.add(cutButton   = new JButton(new ImageIcon(this.getClass().getResource("images/cut.gif"))));
+		toolBar.add(copyButton  = new JButton(new ImageIcon(this.getClass().getResource("images/copy.gif"))));
+		toolBar.add(pasteButton = new JButton(new ImageIcon(this.getClass().getResource("images/paste.gif"))));
+
+		cutButton.setToolTipText("Cut");
+		copyButton.setToolTipText("Copy");
+		pasteButton.setToolTipText("Paste");
+
+		cuT.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				actions.cuT();
+			}
+		});
+		copY.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				actions.copY();
+			}
+		});
+		pastE.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				actions.pastE();
+			}
+		});
+
+		cutButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				actions.cuT();
+			}
+		});
+		copyButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				actions.copY();
+			}
+		});
+		pasteButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				actions.pastE();
+			}
+		});
+
+	
 	    ediT.add(undoAction);
 	    ediT.add(redoAction);
 
@@ -317,6 +366,9 @@ public class  Notepad  extends JFrame {
         new Notepad();
 
     }
+
+	
+	private JButton cutButton, copyButton, pasteButton;
 
 	
     //for using undo & redo
