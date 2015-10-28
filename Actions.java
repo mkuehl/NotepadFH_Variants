16828df5c9ded9493c3625a0f9319e410561a2b5
mkuehl <maxmimilianlkuehl@web.de>
2015-10-28 18:14:05 +0100

    Variant08

--- Actions.java
+++ Actions.java
@@ -462,6 +462,24 @@ public class  Actions {
 	}
 
 	
+	//to cut the selected text
+	public void cuT(){
+		n.getTextArea().cut();
+	}
+
+	
+	//to copy the selected text
+	public void copY(){
+		n.getTextArea().copy();
+	}
+
+	
+	//to paste the selected text
+	public void pastE(){
+		n.getTextArea().paste();
+	}
+
+	
     private String findword;
 
 	 //for searching & finding the word
